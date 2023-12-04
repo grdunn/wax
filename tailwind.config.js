2908604e57f8079ee281a60b2f5ce1dc970ddfe2
Greg Dunn <grdunn@gmail.com>
2023-12-04 13:24:33 -0500

    max width on container

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,15 @@ export default {
         center: true,
       },
     },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1280px",
+    },
   },
+
   extend: {
     fontFamily: {
       sans: ["Overpass"],
